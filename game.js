2e55fd502b6741af089b0b98f9d6dba862d1f4bc
Ray Pearcy <rrpearcy@icloud.com>
2022-10-23 10:30:26 -0500

    added functionality to do best of 5 rounds

--- game.js
+++ game.js
@@ -1,26 +1,48 @@
 function getComputerChoice() {
     const compChoice = ['rock', 'paper', 'scissors'];
     return compChoice[Math.floor(Math.random() * compChoice.length)];
-    
+
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "Tie! You both picked " + playerSelection;
-     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
         return "You win! Rock beats Scissors";
-     } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
         return "You win! Paper beats Rock";
-     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
         return "You win! Scissors beats Paper";
-     } else {
+    } else {
+        compScore += 1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
-     }
+    }
 }
 
-const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log("Player: " + playerSelection);
-console.log("Computer: " + computerSelection);
+let playerScore = 0;
+let compScore = 0;
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    while (playerScore < 3 && compScore < 3) {
+        const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log("Player: " + playerSelection);
+        console.log("Computer: " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    winner();
+}
+
+function winner() {
+    if (compScore === 3) {
+        console.log("The computer wins the match! Better luck next time!");
+    }
+     else  {
+        console.log("You win the match! Way to go!")
+    }
+}
+
+game();
